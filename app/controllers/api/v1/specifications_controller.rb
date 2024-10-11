617ba574c00bbe88d207f22bd29a37caa3390cc6
Hossain <arafat.kd99@gmail.com>
2024-10-11 06:45:11 +0100

    Fix database setup and insert initial data

--- app/controllers/api/v1/specifications_controller.rb
+++ app/controllers/api/v1/specifications_controller.rb
@@ -4,23 +4,4 @@ class Api::V1::SpecificationsController < ApplicationController
     render json: specifications
   end
 
-  def show
-    specification = Specification.find(params[:id])
-    render json: specification
-  end
-
-  def create
-    specification = Specification.new(specification_params)
-    if specification.save
-      render json: specification, status: :created
-    else
-      render json: specification.errors, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def specification_params
-    params.require(:specification).permit(:name)
-  end
 end
